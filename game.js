b33d4285eb5d8e6ee1ca1ae6741b6a8f9f0f2d51
Nurali Khoja <nuralikhoja@gmail.com>
2019-12-20 16:29:26 +0530

    board setup

--- game.js
+++ game.js
@@ -1,3 +1,26 @@
 "use strict";
-console.log('Hello World');
-var x = 6;
+var board = document.querySelector('.board');
+function createBox(i) {
+    var box = document.createElement('td');
+    box.classList.add('box');
+    box.setAttribute('id', i.toString());
+    box.addEventListener('click', clicked);
+    return box;
+}
+function createBoard() {
+    var table = document.createElement('table');
+    for (var i = 0; i < 3; i++) {
+        var row = document.createElement('tr');
+        row.classList.add('row');
+        for (var j = 0; j < 3; j++) {
+            row.appendChild(createBox(j + (i * 3)));
+        }
+        table.appendChild(row);
+    }
+    board.appendChild(table);
+}
+function clicked(e) {
+    var box = e.target;
+    console.log("" + box.id);
+}
+createBoard();
--- game.ts
+++ game.ts
@@ -1,2 +1,29 @@
-console.log('Hello World');
-const x = 6;
+const board: HTMLDivElement = document.querySelector<HTMLDivElement>('.board') as HTMLDivElement;
+
+function createBox(i: number): HTMLDivElement {
+    const box: HTMLDivElement = document.createElement('td');
+    box.classList.add('box');
+    box.setAttribute('id', i.toString());
+    box.addEventListener('click', clicked);
+    return box;
+}
+
+function createBoard() {
+    const table: HTMLTableElement = document.createElement('table');
+    for (let i = 0; i < 3; i++) {
+        const row: HTMLDivElement = document.createElement('tr');
+        row.classList.add('row')
+        for (let j = 0; j < 3; j++) {
+            row.appendChild(createBox(j + (i * 3)));
+        }
+        table.appendChild(row);
+    }
+    board.appendChild(table);
+}
+
+function clicked(e: Event) {
+    let box = e.target as HTMLDivElement;
+    console.log(`${box.id}`);
+}
+
+createBoard();
